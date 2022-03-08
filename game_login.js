91d8b37012dff2e8848514419ca0bda2c2143f63
saahirp <92862778+saahirp@users.noreply.github.com>
2022-03-08 19:56:22 +0800

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,11 @@
+    
+function registerUser()
+{
+    player1name = document.getElementById("player1_name_input").value;
+    player2name = document.getElementById("player2_name_input").value;
+
+    localStorage.setItem("player1_name", player1name);
+    localStorage.setItem("player2_name", player2name);
+
+    window.location = "game_page.html";
+}
